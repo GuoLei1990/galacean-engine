6a68c899ad1fd72e3ad9408dee9949fb2fe47106
GuoLei1990 <gl3336563@163.com>
2021-09-09 20:04:29 +0800

    feat: add comments

--- packages/design/src/StaticInterfaceImplements.ts
+++ packages/design/src/StaticInterfaceImplements.ts
@@ -1,5 +1,6 @@
 /**
  *  Static interface implements decorator.
+ *  https://stackoverflow.com/questions/13955157/how-to-define-static-property-in-typescript-interface
  */
 export function StaticInterfaceImplements<T>() {
   return <U extends T>(constructor: U) => {
