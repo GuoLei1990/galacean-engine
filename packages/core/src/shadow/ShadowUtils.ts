c9a9abb34cfa85bf03ce63c575f995aa97a903a8
GuoLei1990 <gl3336563@163.com>
2022-11-11 18:33:04 +0800

    refactor: opt code

--- packages/core/src/shadow/ShadowUtils.ts
+++ packages/core/src/shadow/ShadowUtils.ts
@@ -45,8 +45,8 @@ export class ShadowUtils {
   private static _shadowMapScaleOffsetMatrix: Matrix = new Matrix(
     0.5, 0.0, 0.0, 0.0,
     0.0, 0.5, 0.0, 0.0,
-    0.0, 0.0, 1.0, 0.0,
-    0.5, 0.5, 0.0, 1.0
+    0.0, 0.0, 0.5, 0.0,
+    0.5, 0.5, 0.5, 1.0
   );
 
   private static _frustumCorners: Vector3[] = [
