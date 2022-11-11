6feb39977dee8c1730cd9c23a5059f20c8172da4
GuoLei1990 <gl3336563@163.com>
2022-11-11 15:54:40 +0800

    fix: shadow edge

--- packages/core/src/shadow/ShadowUtils.ts
+++ packages/core/src/shadow/ShadowUtils.ts
@@ -14,6 +14,7 @@ import { DirectLight } from "../lighting";
 import { Renderer } from "../Renderer";
 import { RenderContext } from "../RenderPipeline/RenderContext";
 import { TextureFormat } from "../texture";
+import { Utils } from "../Utils";
 import { ShadowResolution } from "./enum/ShadowResolution";
 import { ShadowType } from "./enum/ShadowType";
 import { ShadowSliceData } from "./ShadowSliceData";
@@ -37,6 +38,8 @@ enum FrustumCorner {
  * @internal
  */
 export class ShadowUtils {
+  private static _tempMatrix0: Matrix = new Matrix();
+
   private static _frustumCorners: Vector3[] = [
     new Vector3(),
     new Vector3(),
@@ -390,9 +393,6 @@ export class ShadowUtils {
     return resolution;
   }
 
-  /**
-   * @internal
-   */
   static getShadowBias(light: DirectLight, projectionMatrix: Matrix, shadowResolution: number, out: Vector2): void {
     // Frustum size is guaranteed to be a cube as we wrap shadow frustum around a sphere
     // elements[0] = 2.0 / (right - left)
@@ -414,5 +414,41 @@ export class ShadowUtils {
       normalBias *= kernelRadius;
     }
     out.set(depthBias, normalBias);
+  }
+
+  /**
+   * Apply shadow slice scale and offset
+   */
+  static applySliceTransform(
+    tileSize: number,
+    atlasWidth: number,
+    atlasHeight: number,
+    cascadeIndex: number,
+    atlasOffset: Vector2,
+    outShadowMatrices: Float32Array
+  ): void {
+    const slice = ShadowUtils._tempMatrix0.elements;
+    const oneOverAtlasWidth = 1.0 / atlasWidth;
+    const oneOverAtlasHeight = 1.0 / atlasHeight;
+
+    // Apply scale
+    slice[0] = tileSize * oneOverAtlasWidth;
+    slice[5] = tileSize * oneOverAtlasHeight;
+    // Apply offset
+    slice[12] = atlasOffset.x * oneOverAtlasWidth;
+    slice[13] = atlasOffset.y * oneOverAtlasHeight;
+    slice[1] = 0;
+    slice[2] = 0;
+    slice[2] = 0;
+    slice[4] = 0;
+    slice[6] = 0;
+    slice[7] = 0;
+    slice[8] = 0;
+    slice[9] = 0;
+    slice[11] = 0;
+    slice[14] = 0;
+    slice[10] = slice[15] = 1;
+    const offset = cascadeIndex * 16;
+    Utils._floatMatrixMultiply(slice, outShadowMatrices, offset, outShadowMatrices, offset);
   }
 }
