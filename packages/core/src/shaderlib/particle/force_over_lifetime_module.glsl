f6703ad8ecf840ce2e3afa849f86a8039884bf8e
GuoLei1990 <gl3336563@163.com>
2025-04-07 15:34:58 +0800

    refactor: opt code and fix bug

--- packages/core/src/shaderlib/particle/force_over_lifetime_module.glsl
+++ packages/core/src/shaderlib/particle/force_over_lifetime_module.glsl
@@ -29,39 +29,38 @@
     #endif
 
     // (tHat - t1) * (tHat - t1) * (tHat - t1) * (a2 - a1) / ((t2 - t1) * 6.0) + a1 * (tHat - t1) * (tHat - t1) * 0.5 + v1 * (tHat - t1);
-    float computeDisplacementIntegral(in float tHat, in float t1, in float t2, in float a1, in float a2, in float v1) {
-        float tHatSubT1 = tHat - t1;
-        return tHatSubT1 * tHatSubT1 * tHatSubT1 * (a2 - a1) / ((t2 - t1) * 6.0) + a1 * tHatSubT1 * tHatSubT1 * 0.5 + v1 * tHatSubT1;
+    // to * to * to * (a2 - a1) / (tr * 6.0) + a1 * to * to * 0.5 + v1 * to;
+    float computeDisplacementIntegral(in float to, in float tr, in float a1, in float a2, in float v1) {
+        return to * to * to * (a2 - a1) / (tr * 6.0) + a1 * to * to * 0.5 + v1 * to;
     }
 
-    float evaluateForceParticleCurveCumulative(in vec2 keys[4], in float normalizedAge, out float velocityOffsetComponent) {
+    float evaluateForceParticleCurveCumulative(in vec2 keys[4], in float normalizedAge, out float currentVelocity) {
         float cumulativeValue = 0.0;
-        velocityOffsetComponent = 0.0;
+        currentVelocity = 0.0;
 
         for (int i = 1; i < 4; i++){
             vec2 key = keys[i];
             vec2 lastKey = keys[i - 1];
-            float t1 = lastKey.x * a_ShapePositionStartLifeTime.w;
-            float t2 = key.x * a_ShapePositionStartLifeTime.w;
 
+            float normalizedTimeRange = key.x - lastKey.x;
+            float timeRange = normalizedTimeRange * a_ShapePositionStartLifeTime.w;
             if (key.x >= normalizedAge){
-                float tHat = normalizedAge * a_ShapePositionStartLifeTime.w;
-                cumulativeValue += computeDisplacementIntegral(tHat, t1, t2, lastKey.y, key.y, velocityOffsetComponent);
+                float timeOffset = (normalizedAge - lastKey.x) * a_ShapePositionStartLifeTime.w;
+                cumulativeValue += computeDisplacementIntegral(timeOffset, timeRange, lastKey.y, key.y, currentVelocity);
 
-                float timeOffset = normalizedAge - lastKey.x;
-                float ratio = timeOffset / (key.x - lastKey.x);
-                float finalAcceleration = mix(lastKey.y, key.y, ratio);
-                velocityOffsetComponent += 0.5 * timeOffset * (finalAcceleration + lastKey.y);
+                float normalizedTimeOffset = normalizedAge - lastKey.x;
+                float acceleration = mix(lastKey.y, key.y, normalizedTimeOffset / normalizedTimeRange);
+                currentVelocity += 0.5 * normalizedTimeOffset * (lastKey.y + acceleration);
                 break;
             } else {  
-                cumulativeValue += computeDisplacementIntegral(t2, t1, t2, lastKey.y, key.y, velocityOffsetComponent);
-                velocityOffsetComponent += 0.5 * (t2 - t1) * (lastKey.y + key.y);
+                cumulativeValue += computeDisplacementIntegral(timeRange, timeRange, lastKey.y, key.y, currentVelocity);
+                currentVelocity += 0.5 * normalizedTimeRange * (lastKey.y + key.y);
             }
         }
         return cumulativeValue;
     }
 
-    vec3 computeForcePositionOffset(in float normalizedAge, in float age, out vec3 velocityOffset) {
+    vec3 computeForcePositionOffset(in float normalizedAge, in float age, out vec3 currentVelocity) {
         vec3 forcePosition;
 
         #if defined(RENDERER_FOL_CONSTANT_MODE)
@@ -71,25 +70,25 @@
                 forceAcceleration = mix(renderer_FOLMinConst, forceAcceleration, vec3(a_Random2.x, a_Random2.y, a_Random2.z));
             #endif
 
-            velocityOffset = forceAcceleration * age;
+            currentVelocity = forceAcceleration * age;
 
             forcePosition = 0.5 * forceAcceleration * age * age;
         #elif defined(RENDERER_FOL_CURVE_MODE)
             forcePosition = vec3(
-                evaluateForceParticleCurveCumulative(renderer_FOLMaxGradientX, normalizedAge, velocityOffset.x),
-                evaluateForceParticleCurveCumulative(renderer_FOLMaxGradientY, normalizedAge, velocityOffset.y),
-                evaluateForceParticleCurveCumulative(renderer_FOLMaxGradientZ, normalizedAge, velocityOffset.z)
+                evaluateForceParticleCurveCumulative(renderer_FOLMaxGradientX, normalizedAge, currentVelocity.x),
+                evaluateForceParticleCurveCumulative(renderer_FOLMaxGradientY, normalizedAge, currentVelocity.y),
+                evaluateForceParticleCurveCumulative(renderer_FOLMaxGradientZ, normalizedAge, currentVelocity.z)
             );
             #ifdef RENDERER_FOL_IS_RANDOM_TWO
-                vec3 minVelocityOffset;
+                vec3 minCurrentVelocity;
 
                 forcePosition = vec3(
-                    mix(evaluateForceParticleCurveCumulative(renderer_FOLMinGradientX, normalizedAge, minVelocityOffset.x), forcePosition.x, a_Random2.x),
-                    mix(evaluateForceParticleCurveCumulative(renderer_FOLMinGradientY, normalizedAge, minVelocityOffset.y), forcePosition.y, a_Random2.y),
-                    mix(evaluateForceParticleCurveCumulative(renderer_FOLMinGradientZ, normalizedAge, minVelocityOffset.z), forcePosition.z, a_Random2.z)
+                    mix(evaluateForceParticleCurveCumulative(renderer_FOLMinGradientX, normalizedAge, minCurrentVelocity.x), forcePosition.x, a_Random2.x),
+                    mix(evaluateForceParticleCurveCumulative(renderer_FOLMinGradientY, normalizedAge, minCurrentVelocity.y), forcePosition.y, a_Random2.y),
+                    mix(evaluateForceParticleCurveCumulative(renderer_FOLMinGradientZ, normalizedAge, minCurrentVelocity.z), forcePosition.z, a_Random2.z)
                 );
 
-                velocityOffset = mix(minVelocityOffset, velocityOffset, vec3(a_Random2.x, a_Random2.y, a_Random2.z));
+                currentVelocity = mix(minCurrentVelocity, currentVelocity, vec3(a_Random2.x, a_Random2.y, a_Random2.z));
             #endif
         #endif
         return forcePosition;
